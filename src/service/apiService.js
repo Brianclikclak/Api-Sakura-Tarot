755e9ecd789edb1c5128f54e695e08cebccff162
Brianclikclak <brianclikclak@hotmail.com>
2023-07-03 21:55:25 +0200

    update api call

--- src/service/apiService.js
+++ src/service/apiService.js
@@ -4,14 +4,9 @@ const apiUrl = "https://6388b6e5a4bb27a7f78f96a5.mockapi.io/sakura-cards/";
 
 export async function getCardSakuraList() {
   try {
-    const sakuraCards = [];
-    for (let i = 0; i <= 56; i++) {
-      const response = await axios.get(apiUrl + i);
-      const sakura = response.data;
-      sakuraCards.push(sakura);
-      console.log(sakuraCards);
-    }
-    return sakuraCards;
+    const response = await axios.get(apiUrl);
+    const cards = response.data;
+    return cards;
   } catch (error) {
     console.log("No se pudo obtener los datos", error);
     return [];
