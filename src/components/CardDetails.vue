485936a641085ee4353e49e8f7903122a34d6eb5
Brianclikclak <brianclikclak@hotmail.com>
2023-07-04 14:04:39 +0200

    CardDetails component created

--- src/components/CardDetails.vue
+++ src/components/CardDetails.vue
@@ -3,9 +3,17 @@
 </script>
 
 <template>
-<h1>hola</h1>
+    <div>
+<h1>hola Mundo</h1>
+</div>
+
+<button><RouterLink to="/">Reiniciar lectura </RouterLink></button>
+<!-- <RouterView/> -->
 </template>
 
-<style>
+<style scoped>
+h1{
+    background-color: aqua;
+}
 
 </style>
